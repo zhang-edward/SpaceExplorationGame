ba257bc2d397fc61e4cfca4bf09cc6d84fecc86c
David Zhang <david8zhang@berkeley.edu>
2015-12-20 19:13:12 -0800

    cleared GameManager

--- Assets/Scripts/Controllers/GameManager.cs
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -5,45 +5,4 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 
-	private Ship ship;
-	private Starmap starmap;
-	private Shop shop;
-	private CombatManager combat;
-
-	private int turn = 0;
-
-	void Awake()
-	{
-		// make this 
-		if (instance != null)
-			Destroy (gameObject);
-		else
-			instance = this;
-
-		DontDestroyOnLoad();
-	}
-
-	public void Init()
-	{
-		ship.Init();
-		starmap.Init();
-		shop.Init();
-	}
-
-	public void ProcessCrew()
-	{
-		turn ++;
-	}
-
-	public void Travel(Planet planet)
-	{
-		ship.planet = planet;
-		turn ++;
-	}
-
-	public void SetBase()
-	{
-		ship.planet.ToggleBase();
-		turn ++;
-	}
 }
