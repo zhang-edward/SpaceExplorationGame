c9db0fd217394095619170925b1618f91b1572a9
David Zhang <david8zhang@berkeley.edu>
2015-12-21 16:11:37 -0800

    added crew

--- Assets/Scripts/Models/Ship.cs
+++ Assets/Scripts/Models/Ship.cs
@@ -7,7 +7,7 @@ public class Ship : MonoBehaviour {
 	/// <summary>
 	/// The crew.
 	/// </summary>
-	public List<Crew> crewList { get; set; }
+	private List<Crew> crew;
 
 	/// <summary>
 	/// The money.
@@ -22,7 +22,26 @@ public class Ship : MonoBehaviour {
 	/// <summary>
 	/// The planet.
 	/// </summary>
-	public Planet planet{ get; set; }
+	private Planet planet;
+
+	/// <summary>
+	/// The crew prefab.
+	/// </summary>
+	private GameObject crewPrefab;
+
+	/// <summary>
+	/// Initialize this instance
+	/// </summary>
+	void Start() {
+		for (int i = 0; i < 2; i++) {
+			GameObject gobj = Instantiate(crewPrefab, transform.position, Quaternion.identity) as GameObject;
+			Crew newCrew = gobj.GetComponent<Crew>();
+			newCrew.Name = "Crew " + i;
+			newCrew.Health = 10;
+			newCrew.Strength = 5;
+			crew.Add(newCrew);
+		}
+	}
 
 	/// <summary>
 	/// Adds the resources.
@@ -39,5 +58,35 @@ public class Ship : MonoBehaviour {
 	/// <param name="amt">Amt.</param>
 	public void AddMoney(int amt) {
 		money += amt;
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Ship"/> class.
+	/// </summary>
+	public Planet GetPlanet() {
+		return planet;
+	}
+
+	/// <summary>
+	/// Sets the planet.
+	/// </summary>
+	public void SetPlanet(Planet planet) {
+		this.planet = planet;
+	}
+
+	/// <summary>
+	/// Gets the crew.
+	/// </summary>
+	/// <returns>The crew.</returns>
+	public List<Crew> GetCrew() {
+		return crew;
+	}
+
+	/// <summary>
+	/// Sets the crew.
+	/// </summary>
+	/// <param name="crew">Crew.</param>
+	public void SetCrew(List<Crew> crew) {
+		this.crew = crew;
 	}
 }
